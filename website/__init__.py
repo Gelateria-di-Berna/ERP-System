76435a0df12f354d096c07119f33ce075b22b172
Zediwan <Jeremy@qualit.ch>
2024-04-11 10:30:40 +0200

    First runnable version of website

---
+++ website/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+def create_app():
+    app = Flask(__name__)
+    
+    with open("secret_key.txt", 'r') as file:
+        secret_key = file.read().strip() 
+    app.config["SECRET_KEY"] = secret_key
+    
+    return app
